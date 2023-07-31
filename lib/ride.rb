6fdc95c2c4965f6a4473bd7552b711cad8ebd0c6
Will Weston <Williamweston81@gmail.com>
2023-07-31 10:41:00 -0500

    Weston(1) Update total_rev

--- lib/ride.rb
+++ lib/ride.rb
@@ -5,6 +5,14 @@ class Ride
       @min_height = attributes[:min_height]
       @admission_fee = attributes[:admission_fee]
       @excitement = attributes[:excitement]
-      @total_revenue = 0
+      @rider_log = []
+    end
+
+    def total_revenue
+      # pass for now until log with keys and values for amount of times they rode
+      0
+    end
+
+    def board_rider(visitor)
     end
   end
--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -5,6 +5,8 @@ require 'pry'
 RSpec.describe Ride do
   before(:each) do
     @ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+    @visitor1 = Visitor.new('Bruce', 54, '$10')
+    @visitor2 = Visitor.new('Tucker', 36, '$5')
   end
 
   it 'ride exists with symbyol attributes' do
@@ -14,5 +16,18 @@ RSpec.describe Ride do
     expect(@ride1.admission_fee).to eq(1)
     expect(@ride1.excitement).to eq(:gentle)
     expect(@ride1.total_revenue).to eq(0)
+  end
+
+  xit 'adds preference of visitors and boards them for rider_log' do
+     @visitor1.add_preference(:gentle)
+     @visitor2.add_preference(:gentle)
+
+     @ride1.board_rider(visitor1)
+     @ride1.board_rider(visitor2)
+     @ride1.board_rider(visitor1)
+
+    #  expected_hash = {@visitor1, @visitor2}
+
+     expect(@ride1.rider_log).to eq(expected_hash)
   end
 end
