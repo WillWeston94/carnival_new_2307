83a0434d35a67ed760340e0f18ec4b7e05db4080
Will Weston <Williamweston81@gmail.com>
2023-07-31 11:26:37 -0500

    Weston(2) Combine tests to track spending_money due to testing environment Test ride3 ride_log all expects - pass

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -21,7 +21,7 @@ RSpec.describe Ride do
     expect(@ride1.total_revenue).to eq(0)
   end
 
-  it 'adds preference of visitors and boards them for rider_log ride1' do
+  it 'adds preference of visitors and boards them for rider_log rides' do
     @visitor1.add_preference(:gentle)
     @visitor2.add_preference(:gentle)
 
@@ -40,10 +40,8 @@ RSpec.describe Ride do
     expect(@visitor1.spending_money).to eq(8)
     expect(@visitor2.spending_money).to eq(4)
     expect(@ride1.total_revenue).to eq(3)
-  end
 
-  it 'adds preference of visitors and boards them for rider_log ride 3' do
-    @visitor2.add_preference(:thrilling)
+    @visitor2.add_preference(:thrilling) #change from gentle to thrilling
     @visitor3.add_preference(:thrilling)
 
     @ride3.board_rider(@visitor1)
@@ -51,17 +49,15 @@ RSpec.describe Ride do
     @ride3.board_rider(@visitor3)
 
      expected_hash = 
-     {
-      @visitor1 => 2 ,
-      @visitor2 => 1
+      {
+      @visitor3 => 1
       }
     # binding.pry
     expect(@ride3.rider_log).to be_a Hash
-    # expect(@visitor1.spending_money).to eq(8)
-    # expect(@visitor2.spending_money).to eq(4)
-    # expect(@visitor3.spending_money).to eq(13)
+    expect(@visitor1.spending_money).to eq(8) # stored only because this test recognizes the rider rode another ride during this same test
+    expect(@visitor2.spending_money).to eq(4) # possible refactor to store even outside of test environment?
+    expect(@visitor3.spending_money).to eq(13)
     expect(@ride3.rider_log).to eq(expected_hash)
+    expect(@ride3.total_revenue).to eq(2)
   end
-
-
 end
